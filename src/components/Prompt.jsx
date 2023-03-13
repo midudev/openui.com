f52fc16e1e3dcfca1fc94896cf0e2a19e9d98309
Miguel Ángel Durán <miduga@gmail.com>
2023-03-13 01:37:30 +0100

    Better prompt

--- src/components/Prompt.jsx
+++ src/components/Prompt.jsx
@@ -4,7 +4,7 @@ import { Loading } from './Loading'
 import { SendIcon } from './Icons'
 
 export function Prompt() {
-  const textAreaRef = useRef()
+  const inputRef = useRef()
   const generateComponent = useConversationsStore(
     (state) => state.generateComponent
   )
@@ -13,7 +13,7 @@ export function Prompt() {
   async function handleSubmit(event) {
     event.preventDefault()
 
-    const prompt = textAreaRef.current.value.trim()
+    const prompt = inputRef.current.value.trim()
     generateComponent({ prompt })
   }
 
@@ -24,34 +24,25 @@ export function Prompt() {
     }
   }
 
-  const handleChange = (event) => {
-    const el = textAreaRef.current
-
-    el.style.height = '0px'
-    const scrollHeight = el.scrollHeight
-    el.style.height = `${scrollHeight}px`
-  }
-
   useEffect(() => {
-    textAreaRef.current.focus()
+    inputRef.current.focus()
   }, [])
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className='relative block max-w-xl md:max-w-2xl'>
-        <textarea
+      <div className='relative block'>
+        <input
           onKeyDown={handleKeyDown}
-          onChange={handleChange}
           disabled={streaming}
-          ref={textAreaRef}
+          ref={inputRef}
           autoFocus
           placeholder='Crea un botón de color rojo con un borde de 2px y un borde redondeado de 5px.'
           rows={1}
           name='prompt'
           type='text'
-          className={`resize-none ${
-            streaming ? 'placeholder-white rounded-[5px] opacity-40 pointer-events-none' : ''
-          } block w-full text-md px-4 py-2 border border-gray-800 bg-zinc-900/20 backdrop-blur-3xl sm:text-md shadow-lg h-[42px] text-white outline-none overflow-hidden transition ring-white/20 focus:ring-2`}
+          className={`resize-none pr-10 ${
+            streaming ? 'opacity-40 pointer-events-none' : ''
+          } placeholder-white/30 rounded-2xl block w-full text-md px-6 text-xl py-4 border border-zinc-600 bg-white/5 backdrop-blur-3xl sm:text-md shadow-lg text-white outline-none overflow-hidden transition ring-white/40 focus:ring-2`}
         />
         <div className='absolute top-0 flex items-center justify-center h-full right-4'>
           {streaming ? (
