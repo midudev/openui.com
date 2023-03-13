6f1b36b5b6cf61c93bd5457a3cd8f4209d1c57c0
Miguel Ángel Durán <miduga@gmail.com>
2023-03-13 01:38:11 +0100

    Add Toaster

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import { Toaster } from 'sonner'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Toaster position='bottom-right' />
+      <Component {...pageProps} />
+    </>
+  )
 }
